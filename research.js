904983d67ab90c92c16d323e3bbd3261cfd90789
Otsuka Haruki <bjho1290429@gn.iwasaki.ac.jp>
2022-07-13 14:57:49 +0900

    update file

---
+++ research.js
@@ -0,0 +1,274 @@
+// Cookie登録フラグ
+let flg_cookie = '';
+
+// APIから受け取った店舗数
+let shop_length = '';
+
+
+//店舗詳細画面用のCookie登録
+const post = (jsonData) => {
+  jsonData.forEach((shop_info, shop_info_index) => {
+    document.cookie = 'id' + shop_info_index + '=' + shop_info['id'];
+    document.cookie = 'shop_name' + shop_info_index + '=' + shop_info['name'];
+    document.cookie = 'address' + shop_info_index + '=' + shop_info['address'];
+    document.cookie = 'business_hours' + shop_info_index + '=' + shop_info['open'];
+    document.cookie = 'image' + shop_info_index + '=' + shop_info['photo']['pc']['l'];
+    document.cookie = 'budget' + shop_info_index + '=' + shop_info['budget']['name'];
+    document.cookie = 'genre' + shop_info_index + '=' + shop_info['genre']['name'];
+    document.cookie = 'genre_catch_copy' + shop_info_index + '=' + shop_info['genre']['catch'];
+    document.cookie = 'capacity' + shop_info_index + '=' + shop_info['capacity'];
+    document.cookie = 'close' + shop_info_index + '=' + shop_info['close'];
+    document.cookie = 'lunch' + shop_info_index + '=' + shop_info['lunch'];
+  })
+}
+
+// ページング機能
+let pagination = (jsonData) => {
+
+    // Cookieに登録していなければ、登録する
+    if ( !flg_cookie ) {
+      post(jsonData); //APIのjsonデータをクッキーに登録する
+      flg_cookie = 1;
+    }
+    // 初期値設定
+    let page = 1; // 現在のページ（何ページ目か）
+    const step = 5; // ステップ数（1ページに表示する店舗数）
+
+    // 現在のページ/全ページ を表示
+    // <p class="count"></p> の中身を書き換え
+    let count = (page, step) => {
+        const page_count = document.querySelector('.count');
+        // 全ページ数　店舗の総数/ステップ数の余りの有無で場合分け
+        let total = (shop_length % step == 0) ? (shop_length / step) : (Math.floor(shop_length / step) + 1);
+        page_count.innerText = page + "/" + total + "ページ";
+    }
+
+    // ページを表示
+    // <ul class="menu_list"></ul> の中身を書き換え
+    let show = (page, step) => {
+
+      // 「<div class="shops_container"></div>」に検索結果を入れる
+      const div_shops_container = document.getElementById('shops_container');
+
+      // HTMLに表示されている店舗情報を空にする
+      while (div_shops_container.lastChild) {
+          div_shops_container.removeChild(div_shops_container.lastChild);
+      }
+
+      let first = (page - 1) * step + 1;
+      let last = page * step;
+
+      // HTMLに店舗情報を挿入する
+      jsonData.forEach((item, i) => {
+
+          // 1ページ目を開いていて、戻るボタン（<）と先頭に戻るボタン（<<）を押された場合と、
+          // 最後のページを開いていて、次のボタン（>）と最後に行くボタン（>>）を押されたら何もしない
+          if(i < first - 1 || i > last - 1) return;
+
+          const div_shop_item = document.createElement('div');
+          div_shop_item.classList.add('div_shop_item', 'row');
+
+          const div_shop_item_img = document.createElement('div');
+          const div_shop_item_info = document.createElement('div');
+          div_shop_item_img.classList.add('shop_item_img', 'col-md-6');
+          div_shop_item_info.classList.add('shop_item_info', 'col-md-6');
+
+          // 「<div class="shop_item_img col-md-6">」の中に、「<div class="img_css">」「<img id="shop_image">」を入れる
+          const div_img_css = document.createElement('div');
+          const img_shop_image = document.createElement('img');
+          img_shop_image.setAttribute("id", 'shop_image');
+          img_shop_image.src = item['photo']['pc']['l'];
+          div_img_css.appendChild(img_shop_image);
+          div_shop_item_img.appendChild(div_img_css);
+
+          // 「<div class="div_shop_item row">」の中に、「<div class="shop_item_img col-md-6">」を入れる
+          div_shop_item.appendChild(div_shop_item_img);
+
+          // 「<div class="div_shop_item_info">」の中に、「<h3>」「<p>」「<a>」を入れる
+          const h3_shop_name = document.createElement('h3');
+          h3_shop_name.textContent = '店舗名';
+          div_shop_item_info.appendChild(h3_shop_name);
+
+          const p_shop_name = document.createElement('p');
+          p_shop_name.innerText = item['name'];
+          div_shop_item_info.appendChild(p_shop_name);
+
+          const h3_shop_access = document.createElement('h3');
+          h3_shop_access.textContent = 'アクセス';
+          div_shop_item_info.appendChild(h3_shop_access);
+
+          const p_shop_access = document.createElement('p');
+          p_shop_access.innerText = item['access'];
+          div_shop_item_info.appendChild(p_shop_access);
+
+          const h3_shop_address = document.createElement('h3');
+          h3_shop_address.textContent = '住所';
+          div_shop_item_info.appendChild(h3_shop_address);
+
+          const p_shop_address = document.createElement('p');
+          p_shop_address.innerText = item['address'];
+          div_shop_item_info.appendChild(p_shop_address);
+
+          // 店舗詳細ページ「shop.php」へのURLを作成
+          const div_a_shop_explan = document.createElement('div');
+          div_a_shop_explan.classList.add('div_a_btn');
+          const a_shop_explan = document.createElement('a');
+          a_shop_explan.setAttribute("id", i);
+          a_shop_explan.classList.add('a_btn_css');
+          a_shop_explan.setAttribute("target", '_blank');
+          a_shop_explan.setAttribute("rel", 'noopener noreferrer');
+          a_shop_explan.href = './shop.php' + '/' + item['id']; // 店舗IDをURLに付ける（付けたIDとクッキー情報の店舗IDをマッチさせるため）
+          a_shop_explan.textContent = '詳細'
+          div_a_shop_explan.appendChild(a_shop_explan);
+          div_shop_item_info.appendChild(div_a_shop_explan);
+
+          // 「<div class="shop_item row">」の中に、「<div class="div_shop_item_info">」を入れる
+          div_shop_item.appendChild(div_shop_item_info);
+
+          // 「<div class="shops_container">」の中に、「<div class="div_shop_item row">」を入れる
+          div_shops_container.appendChild(div_shop_item);
+
+      });
+      count(page,step);
+    }
+
+  // 最初に1ページ目を表示
+  show(page, step);
+
+  // 最前ページ遷移トリガー
+  document.getElementById('first').addEventListener('click', () => {
+      if(page <= 1) return;
+      page = 1;
+      // const a_last_btn = document.getElementById('last_btn');
+      // a_last_btn.href = '#'; // 店舗情報を書き換える時に、ページトップに戻る
+      show(page, step);
+  });
+
+  // 前ページ遷移トリガー
+  document.getElementById('prev').addEventListener('click', () => {
+      if(page <= 1) return;
+      page = page - 1;
+      // const a_last_btn = document.getElementById('last_btn');
+      // a_last_btn.href = '#'; // 店舗情報を書き換える時に、ページトップに戻る
+      show(page, step);
+  });
+
+  // 次ページ遷移トリガー
+  document.getElementById('next').addEventListener('click', () => {
+      if(page >= Math.floor(shop_length / step) + 1) return;
+      page = page + 1;
+      // const a_last_btn = document.getElementById('last_btn');
+      // a_last_btn.href = '#'; // 店舗情報を書き換える時に、ページトップに戻る
+      show(page, step);
+  });
+
+  // 最終ページ遷移トリガー
+  document.getElementById('last').addEventListener('click', () => {
+      if(page >= Math.floor(shop_length / step) + 1) return;
+      page = Math.floor(shop_length / step) + 1;
+      // const a_last_btn = document.getElementById('last_btn');
+      // a_last_btn.href = '#'; // 店舗情報を書き換える時に、ページトップに戻る
+      show(page, step);
+  });
+
+}
+
+
+// メイン処理
+const button = document.getElementById('location-info-btn');
+button.addEventListener('click', () => {
+
+  // Cookie情報削除
+  let cookies = document.cookie;
+  let cookiesArray = cookies.split(';'); // Cookieを1つずつ配列に入れる
+  for (let cookie of cookiesArray) {
+    let cookie_array = cookie.split('=');
+    document.cookie = cookie_array[0] + '=; max-age=0' // 1つずつクッキーの有効期限を0にする（つまり削除）
+  }
+
+  // HTMLに挿入されている店舗情報を削除
+  const div_shops_container = document.getElementById('shops_container');
+  while (div_shops_container.lastChild) {
+    div_shops_container.removeChild(div_shops_container.lastChild);
+  }
+
+  // 初期値
+  const status = document.querySelector(".status");
+  const research_error = document.getElementById('research_error');
+  research_error.textContent = ''
+  let range = ranges.value;
+  const count = 15; // 最大15件紹介する
+  flg_cookie = ''; // Cookie登録フラグを空にしておく
+
+  // 隠してある要素（class="hiddin_item"）を表示させる
+  document.getElementById("h2_hiddin_item").classList.remove("hiddin_item");
+  document.getElementById("ul_hiddin_item").classList.remove("hiddin_item");
+
+  // ブラウザがGeolocation APIに対応しているかをチェック
+  if (!navigator.geolocation) {
+    status.textContent = "ブラウザがGeolocationに対応していません";
+    // 対応している → 位置情報取得開始
+    // 位置情報取得成功時にsuccess()、そして取得失敗時にerror()がコールされる
+    // optionsはgetCurrentPosition()に渡す設定値
+  } else {
+    status.textContent = "Locating…";
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+
+  // 位置情報取得処理が成功した時にコールされる関数
+  // 引数として、coords(coordinates。緯度・経度など)とtimestamp(タイムスタンプ)の2つを持ったpositionが渡される
+  async function success(position) {
+    status.textContent = "";
+    // let latitude = position['coords']['latitude']; // 緯度取得
+    // let longitude = position['coords']['longitude']; // 経度取得
+    let latitude = '36.25518193'; // 緯度取得
+    let longitude = '139.15604137' // 経度取得
+
+    // 検索条件をFormDataに格納する
+    let api_postData = new FormData();
+    api_postData.set("latitude", latitude);
+    api_postData.set("longitude", longitude);
+    api_postData.set("range", range);
+    api_postData.set("count", count);
+
+    let api_data = {
+      method: "POST",
+      body: api_postData,
+    };
+    // FetchApiを使ってグルメサーチAPIからjsonを取得するリクエストをする
+    let res = await fetch("main.php", api_data);
+    let result_data = await res.json();
+    let json = await JSON.parse(result_data);
+    if (json['results']['results_returned'] == '0') {
+      research_error.innerText = '検索範囲内に店舗がありませんでした。\n検索範囲を広げて再検索お願いします。'
+    } else {
+      let jsonData = json['results']['shop'];
+      shop_length = jsonData.length; // 配列数をグローバル変数に入れる
+      pagination(jsonData); // 受け取ったjsonデータを出力する
+    }
+
+  }
+
+  // 位置情報取得処理が失敗した時にコールされる関数
+  // 引数として、code(コード)とmessage(メッセージ)の2つを持ったpositionErrorが渡される
+  function error(positionError) {
+    switch (positionError.code) {
+      case 0: // 0:UNKNOWN_ERROR
+        status.textContent = "原因不明のエラーが発生しました。";
+        break;
+
+      case 1: // 1:PERMISSION_DENIED
+        status.textContent = "  位置情報の取得が許可されませんでした。";
+        break;
+
+      case 2: // 2:POSITION_UNAVAILABLE
+        status.textContent = "電波状況などで位置情報が取得できませんでした。";
+        break;
+
+      case 3: // 3:TIMEOUT
+        status.textContent =
+          "位置情報の取得に時間がかかり過ぎてタイムアウトしました。";
+        break;
+    }
+  }
+});
---
+++ shop.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>現在地付近のレストラン情報</title>
+	<meta name="description" content="現在地付近のレストラン情報を検索するウェブアプリ">
+    <link rel="stylesheet" href="../shop.css">
+    <!-- BootstrapのCSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+	<h1 id="shop_name">店舗名称</h1><hr>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<div class="img_css">
+					<img id="shop_image" src="" alt="">
+				</div>
+			</div>
+			<div class="col-md-6">
+				<h3>住所</h3>
+				<p id="address"></p>
+				<h3>営業時間</h3>
+				<p id="business_hours"></p>
+				<h3>定休日</h3>
+				<p id="close"></p>
+				<h3>料金目安</h3>
+				<p id="budget"></p>
+				<h3>カテゴリー</h3>
+				<p id="genre"></p>
+				<p id="genre_catch_copy"></p>
+				<h3>総席数</h3>
+				<p id="capacity"></p>
+				<h3>ランチ</h3>
+				<p id="lunch"></p>
+			</div>
+			<p class="powered_by">Powered by <a href="http://webservice.recruit.co.jp/">ホットペッパー Webサービス</a></p>
+			<p id="error_text"></p>
+			<p><a id="top_page_url" href=""></a></p>
+		</div>
+	</div>
+    <script type="text/javascript" src="../shop.js"></script>
+    <!-- BootstrapのJavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
